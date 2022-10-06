bd76fe2b7564e094ecd0e828a502f5235a98307e
Gaizka79 <agaizka79@gmail.com>
2022-10-06 23:40:28 +0200

    App OK

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import Nav from './Nav/Nav';
 
-import logo from '../../logo.svg';
+//import logo from '../../logo.svg';
 
 function Header () {
   return (
     <header className="header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Under construction
-            </p>
-            <a
-              className="App-link"
-              href="https://github.com/Gaizka79/converter.git"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Git Hub Project
-            </a>
-      <Nav/>
+      <p className="title">
+        <b>Unit Converter</b> 
+      </p>
+      {/* <Nav/> */}
     </header>
   ) 
      
@@ -27,3 +17,4 @@ function Header () {
 }
 
 export default Header;
+      
--- src/components/Main/Temperatura/Temperatura.jsx
+++ src/components/Main/Temperatura/Temperatura.jsx
@@ -1,34 +1,60 @@
-import React from "react";
-
+import React, { useState } from "react";
 import intercambio from '../../../assets/intercambio.png';
 
-function Temperatura () {
-  return (
-    <div className="temperatura">
-      Temperatura
-      <div className="unidades">
-        <label htmlFor="desde">Desde:</label>
-        <select name="desde" id="desde">
-          <option value="gr">Gramos</option>
-          <option value="lb">Libras</option>
-          <option value="oz">Onzas</option> 
-        </select>
+const convert = require('convert-units');
 
-        <label htmlFor="hasta">Hasta:</label>
-        <select name="hasta" id="hasta">
-          <option value="gr">Gramos</option>
-          <option value="lb">Libras</option>
-          <option value="oz">Onzas</option>
-        </select>
-      </div>
-      <div className="camposdecambio">
-        <input type="text" name="convertir1" id="convertir1" />
-        <button><img src={intercambio} alt="intercambio" className="intercambio" id="intercambio" /></button>
-        <input type="text" name="convertir2" id="convertir2" />
-      </div>
-    </div>
-  )
-  
+function Temperatura () {
+    
+    const [desde, setDesde] = useState(convert().list("temperature")[0].abbr);
+    const [hasta, setHasta] = useState(convert().list("temperature")[1].abbr);
+
+    const [input, setInput] = useState(0);
+    const [cambio, setCambio] = useState(0);
+
+    const handleChangeDesde = (e) => {
+        setInput(e.target.value)
+        setCambio((convert(e.target.value).from(desde).to(hasta)).toFixed(3))
+    }
+    const handleChangeHasta = (e) => {
+        setCambio(e.target.value)
+        setInput((convert(e.target.value).from(hasta).to(desde)).toFixed(3))
+    }
+
+    return (
+        <div className="marcoPrincipal">
+            <p className="campo">Temperatura</p>  
+            <div className="select">
+                <label htmlFor="desde" className="label">Desde:</label>
+                <select className="select" value={desde} onChange={(e) => setDesde(e.target.value)}>
+                    {convert().list("temperature").map((option, i) => (
+                        <option key={i} value={option.abbr}>
+                            {option.singular}
+                        </option>
+                    ))}
+                </select>
+
+                <label htmlFor="hasta" className="label">Hasta:</label>
+                <select className="select" value={hasta} onChange={(e) => setHasta(e.target.value)}>
+                    {convert().list("temperature").map((option, i) => (
+                        <option key={i} value={option.abbr}>
+                            {option.singular}
+                        </option>
+                    ))}
+                </select> 
+            </div>
+
+            <div className="camposdecambio">
+                <input value={input} type="text" className="convertir" onChange={(e) => handleChangeDesde(e)} onFocus={(e) => {e.target.select()}} />
+                <button><img src={intercambio} alt="intercambio" className="intercambio"/></button>
+                <input value={cambio} type="text" className="convertir" onChange={(e) => handleChangeHasta(e)} onFocus={(e) => {e.target.select()}}/>
+            </div>
+
+            <div className="unidades">
+                <p className="unidad">{desde !=="" ?  desde : ""}</p>
+                <p className="unidad">{hasta !=="" ?  hasta : ""}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Temperatura;
